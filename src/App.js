119a968741ec4714ea5bb8144e19422c9d4fd3be
Carolina Nonato <csnonato@gmail.com>
2022-02-18 22:07:33 -0500

    filtering by genre

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { useEffect, useState } from 'react'
 import Movie from './Movie';
+import Filter from './Filter';
 
 function App() {
   const [popular, setPopular] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [activeGenre, setActiveGenre] = useState(0)
+
+
   useEffect(() => {
     fetchPopular()
   }, [])
@@ -12,16 +17,18 @@ function App() {
   const fetchPopular = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=e7b965a1e8c4f16024963fa86c68bd1d&language=en-US&page=1')
     const movies = await data.json()
-    console.log(movies)
+
     setPopular(movies.results)
 
+    setFiltered(movies.results)
   }
 
   return (
 
     <div className='App'>
+      <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre} />
       <div className="popular-movies">
-        {popular.map((movie) => {
+        {filtered.map((movie) => {
           return <Movie key={movie.id} movie={movie} />
 
         })}
